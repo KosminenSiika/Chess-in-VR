fa60d0a0ccb91f7ae2c15a7089c1386808d535f1
Leevi Matias Mäki-Kerttula <leevi221001@gmail.com>
2025-04-29 16:22:05 +0300

    Implement TP onto/out of chair

---
+++ Assets/Scripts/SeatTPBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SeatTPBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject plane;
+    [SerializeField] private GameObject chair;
+
+    [SerializeField] private ActionBasedContinuousMoveProvider moveProvider;
+    [SerializeField] private ActionBasedContinuousTurnProvider turnProvider;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private CharacterControllerDriver characterControllerDriver;
+
+    public void OnTeleportToSeat()
+    {
+        plane.GetComponent<TeleportationAnchor>().enabled = true;
+        chair.GetComponent<TeleportationAnchor>().enabled = false;
+
+        moveProvider.enabled = false;
+        turnProvider.enabled = false;
+        characterControllerDriver.enabled = false;
+        characterController.enabled = false;
+    }
+
+    public void OnTeleportToPlane()
+    {
+        plane.GetComponent<TeleportationAnchor>().enabled = false;
+        chair.GetComponent<TeleportationAnchor>().enabled = true;
+
+        moveProvider.enabled = true;
+        turnProvider.enabled = true;
+        characterController.enabled = true;
+        characterControllerDriver.enabled = true;
+    }
+
+
+}
